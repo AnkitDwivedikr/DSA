1f0595fa52bc5baa17f9e931bc59728afb900512
Ankit Kumar Dwivedi <ankitdwivedikr@gmail.com>
2026-02-10 20:53:14 +0530

    Time: 55 ms (31.58%) | Memory: 29.1 MB (45.47%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end()); // sorting algo
+
+        for(int i=0; i<n-2; i++){
+            // duplicate skip
+            if(i>0 && nums[i]==nums[i-1])
+             continue;
+
+             int left = i+1;
+             int right = n-1;
+  
+             while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+
+                if(sum==0){
+                ans.push_back({nums[i], nums[left], nums[right]});
+
+                while(left<right && nums[left]==nums[left+1]) // skip the duplicate 
+                 left++;  //[-1, -1, -1, 2, 2]  aise test case ko handel karne ke liye hame need hai;
+                while(left<right && nums[right]==nums[right-1]) // skip the duplicate
+                 right--;
+
+                 left++;
+                 right--;
+             }
+                  else if(sum < 0){
+                   left++;
+                  }
+                  else{
+                   right--;
+                  }
+             }
+        }
+        return ans;
+    }
+};
